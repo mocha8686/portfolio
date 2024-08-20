a82d902f26f8d82aa0367086f4bfef27cde684e2
mocha8686 <june.mocha8686@gmail.com>
2024-08-19 18:04:24 -0700

    lint

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,8 @@
+import svelte from '@astrojs/svelte';
+import icon from 'astro-icon';
 import { defineConfig } from 'astro/config';
-import svelte from "@astrojs/svelte";
-
-import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [svelte(), icon()]
+	integrations: [svelte(), icon()],
 });
